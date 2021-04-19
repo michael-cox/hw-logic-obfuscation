0afb6d71f322ea4f608eeb6b582789771ff6b2e2
Michael Cox <mcox59@vols.utk.edu>
2021-04-19 17:28:09 -0400

    add hope opts

---
+++ logic_obf.py
@@ -0,0 +1,9 @@
+#!python3
+
+import argparse
+import subprocess
+
+# -s 500 -r 10 -F faults -l log -U undetected_faults
+HOPE_OPTS = ['-s', '500', '-r', '10', '-F', 'faults', '-l', 'log', '-U', 'undetected_faults']
+
+
