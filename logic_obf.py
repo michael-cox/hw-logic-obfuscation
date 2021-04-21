a613c9c57867592bdd19455e65910dc7048c1afb
Michael Cox <mcox59@vols.utk.edu>
2021-04-20 20:41:45 -0400

    mostly implement to verilog

--- logic_obf.py
+++ logic_obf.py
@@ -5,6 +5,7 @@ import subprocess
 import sys
 import os
 import re
+import pathlib
 
 # -s 500 -r 10 -F faults -l log -U undetected_faults
 HOPE_OPTS = ['./hope/hope', '-s', '500', '-r', '10', '-F', 'faults', '-l', 'log', '-N']
@@ -13,7 +14,18 @@ INPUT_RE = re.compile('^INPUT\((?P<inputs>\w*)\)', flags=re.A | re.M)
 OUTPUT_RE = re.compile('^OUTPUT\((?P<outputs>\w*)\)', flags=re.A | re.M)
 OP_RE = re.compile('^(\w*)\s*=\s*(\w*)\((.*)\)', flags = re.M | re.A)
 
+DFF_GATES = ['VDD', 'CK']
+
 LOGIC_OP_STR = '{} = {}({})'
+
+BENCH_TO_VERILOG = {
+        'DFF' : 'FD',
+        'NOT' : 'IV ',
+        'NAND' : 'ND',
+        'NOR' : 'NR',
+        'AND' : 'AN',
+        'OR' : 'OR'
+        }
 
 # LogicOp - class containing a testbench style logic operation
 # ----------
@@ -23,7 +35,7 @@ LOGIC_OP_STR = '{} = {}({})'
 class LogicOp:
     def __init__(self, assignee, operation, operands):
         self.assignee = assignee
-        self.operation = operation
+        self.operation = operation.upper()
         self.operands = operands
     def __repr__(self):
         return LOGIC_OP_STR.format(self.assignee, self.operation, ', '.join(self.operands))
@@ -39,6 +51,8 @@ class Bench:
     def from_file(netlist):
         with open(netlist, 'r') as data:
             text = data.read()
+        extpos = netlist.rfind('.')
+        name = pathlib.PurePath(netlist).stem
         inputs = INPUT_RE.findall(text)
         outputs = OUTPUT_RE.findall(text)
         op_full_match = OP_RE.findall(text)
@@ -49,9 +63,10 @@ class Bench:
             operands = re.sub(r'\s+', '', operands)
             ops.append(LogicOp(op_reg[0], op_reg[1], operands.split(',')))
     
-        return Bench(inputs, outputs, signals, ops)
+        return Bench(name, inputs, outputs, signals, ops)
 
-    def __init__(self, inputs, outputs, signals, ops):
+    def __init__(self, name, inputs, outputs, signals, ops):
+        self.name = name
         self.inputs = inputs
         self.outputs = outputs
         self.signals = signals
@@ -64,7 +79,48 @@ class Bench:
         print('OPS:')
         for op in self.ops: print(op)
 
-    # TODO: function for converting to verilog
+    def to_verilog(self, outfile):
+        with open(outfile, 'w') as verilog_file:
+            included_dff_gates = []
+            for op in self.ops:
+                if op.operation == 'DFF':
+                    included_dff_gates = DFF_GATES
+                    break
+            print('module {name}({ports});'.format(name=self.name,
+                ports=','.join(included_dff_gates + self.inputs + self.outputs)),
+                file=verilog_file)
+            print('input {inputs};'.format(inputs=','.join(included_dff_gates + self.inputs)),
+                    file=verilog_file)
+            print('output {outputs};'.format(outputs=','.join(self.outputs)),
+                    file=verilog_file)
+            print(file=verilog_file)
+            print('  wire {signals};'.format(signals=','.join(self.signals)),
+                    file=verilog_file)
+            print(file=verilog_file)
+            op_count = {}
+            for op in self.ops:
+                count = 0
+                if op.operation not in op_count:
+                    op_count[op.operation] = 1
+                else:
+                    count = op_count[op.operation]
+                    op_count[op.operation] += 1
+                included_clk = []
+                if op.operation == 'DFF':
+                    included_clk.append('CK')
+                num_operands = 0
+                if op.operation != 'IV':
+                    num_operands = len(op.operands)
+                print('  {module:<3} {mod_name}_{count}({gates});'.format(
+                    module = BENCH_TO_VERILOG[op.operation] + str(num_operands),
+                    mod_name = op.operation,
+                    count = count,
+                    gates = ','.join(included_clk + [op.assignee] + op.operands)),
+                    file=verilog_file)
+            print(file=verilog_file)
+            print('endmodule', file=verilog_file)
+            
+        
     # TODO: function for representing as bench
 
 # error - function to print errors
@@ -104,6 +160,6 @@ if __name__ == '__main__':
     args = parse_args()
     bench = Bench.from_file(args.input_netlist)
     bench.debug_print()
+    bench.to_verilog('out.v')
 
-    hope_out = get_hope_faults(args.input_netlist)
-    print(hope_out)
+    # print(hope_out)
